94817868d380be34122e9a1e4ae9c5b958dd1daa
RustamRaushanovich <imronbekr@gmail.com>
2026-02-12 21:17:54 +0500

    Delete src directory

--- src/config/topics.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    getTopics: () => ({
-        "Farg‘ona shahri": 2, "Marg‘ilon shahri": 3, "Beshariq tumani": 12, "Bag‘dod tumani": 11,
-        "Uchko‘prik tumani": 21, "Qo‘shtepa tumani": 16, "Farg‘ona tumani": 22, "O‘zbekiston tumani": 24,
-        "Dang‘ara tumani": 14, "Rishton tumani": 18, "So‘x tumani": 19, "Toshloq tumani": 20,
-        "Oltiariq tumani": 17, "Furqat tumani": 23, "Buvayda tumani": 13, "Quva tumani": 25,
-        "Qo‘qon shahri": 4, "Quvasoy shahri": 5, "Yozyovon tumani": 15, "Test rejimi": 11220
-    })
-};
